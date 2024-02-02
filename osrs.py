bf456e253a3a318a2647ee9997fcf5929602fadd
Joel Gilger <joelgilger@gmail.com>
2024-02-01 23:35:08 -0800

    added monster check and guesser

--- osrs.py
+++ osrs.py
@@ -1,20 +1,37 @@
-from osrsbox import items_api
+from osrsbox import items_api, monsters_api
+
 
 import random
+
+def load_monsters():
+    # load monsters from osrsbox api
+    monsters = monsters_api.load()
+    monsters_no_dupe = {}
+
+    print("adding monsters to no dupes dictionary")
+    for i in monsters:
+        if i.name not in monsters_no_dupe:
+            monsters_no_dupe[i.name] = i.examine
+
+    return monsters_no_dupe
+
+def random_monster(monsters_no_dupe):
+    # select random monster
+    return random.choice(list(monsters_no_dupe.items()))
 
 def load_items():
     # load items from osrsbox api
     items = items_api.load()
-    items_no_dup = {}
+    items_no_dupe = {}
     
-    print("adding items to no dupes list")
+    print("adding items to no dupes dictionary")
     for i in items:
-        if i.name not in items_no_dup:
-            items_no_dup[i.name] = i.icon
+        if i.name not in items_no_dupe:
+            items_no_dupe[i.name] = i.icon
 
-    return items_no_dup
+    return items_no_dupe
 
-def random_item(items_no_dup):
+def random_item(items_no_dupe):
     # select random item
-    return random.choice(list(items_no_dup.items()))
+    return random.choice(list(items_no_dupe.items()))
 
--- server.py
+++ server.py
@@ -3,17 +3,22 @@ from flask_cors import CORS
 from cryptography.fernet import Fernet
 
 import osrs
+import base64
 
 items = osrs.load_items() #load all items before startup
+monsters = osrs.load_monsters()
 
 item_count = {}
 for item in items:
     item_count.update({item.lower(): 0})
-# print(item_count)
+
+monsters_count = {}
+for monster in monsters:
+     monsters_count.update({monster.lower(): 0})
     
 
-key = Fernet.generate_key() #generate encryption key
-fernet = Fernet(key)
+# key = Fernet.generate_key() #generate encryption key
+# fernet = Fernet(key)
 
 app = Flask(__name__)
 CORS(app)
@@ -24,37 +29,78 @@ def home():
     print(item[0])
     return jsonify({"item": item[0]})
 
+
 #item API Route
 @app.route("/item")
 def get_random_item():
     item = osrs.random_item(items)
     print(item[0])
     
-    encName = fernet.encrypt(item[0].encode())
+    # encName = fernet.encrypt(item[0].encode())
+    encName = base64.b64encode(item[0].encode())
+    return jsonify({"name": encName.decode(), "img": item[1]})
 
-    return jsonify({"name": encName.decode("utf-8"), "img": item[1]})
 
 #guess check API Route
 @app.route("/guesscheck", methods = ['POST'])
-
 def guessCheck():
     
-    guess = request.get_json()
-
-    name = guess["name"] # encrypted
-    response = guess["guess"]
-    decName = fernet.decrypt(name).decode().lower()
-    
-    if response.lower() == decName:
+        guess = request.get_json()
+        name = guess["name"] # encrypted
         
-        if item_count.get(decName) != None:
-            item_count[decName] = item_count.get(decName) + 1 # increase value by 1
-            
-        return jsonify({'correct': True, 'count': item_count[decName]})
+        response = guess["guess"]
+
+        # decName = fernet.decrypt(name).decode()
+        decName = base64.b64decode(name).decode()
+        decName = decName.lower()
+        print(decName)
+        
+        
+        if response.lower() == decName:
+
+            if item_count.get(decName) != None:
+                item_count[decName] = item_count.get(decName) + 1 # increase value by 1
+                
+            return jsonify({'correct': True, 'count': item_count[decName]})
+        else:   
+          
+            return jsonify({'correct': False, 'count': item_count[decName]})
+
+@app.route("/monster")
+def get_random_monster():
+    monster = osrs.random_monster(monsters)
+    print(monster[0])
     
-    else:
-        return jsonify({'correct': False, 'count': item_count[decName]})
     
+    # encName = fernet.encrypt(item[0].encode())
+    encName = base64.b64encode(monster[0].encode())
+    return jsonify({"name": encName.decode(), "img": monster[1]})
+
+#guess check API Route
+@app.route("/guesscheckmonster", methods = ['POST'])
+def guessCheckMonster():
+    
+        guess = request.get_json()
+        name = guess["name"] # encrypted
+        
+        response = guess["guess"]
+
+        # decName = fernet.decrypt(name).decode()
+        decName = base64.b64decode(name).decode()
+        decName = decName.lower()
+        print(decName)
+        
+        
+        if response.lower() == decName:
+
+            if monsters_count.get(decName) != None:
+                monsters_count[decName] = monsters_count.get(decName) + 1 # increase value by 1
+                
+            return jsonify({'correct': True, 'count': monsters_count[decName]})
+        else:   
+          
+            return jsonify({'correct': False, 'count': monsters_count[decName]})
+        
 
 if __name__ == "__main__":
     app.run()
